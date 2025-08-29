a152255d7ebfb4d8cfaeb51e3c50834a0f1f3a68
Nutileety <nutileety@gmail.com>
2025-08-29 11:46:00 +0530

    Setup is done with adding the pages, components and routes as per the need

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Sidebar from './components/Sidebar'
+import { Routes, Route } from 'react-router-dom'
+import Community from './pages/Community'
+import Credits from './pages/Credits'
+import ChatBox from './components/ChatBox'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white'>
+      <div className='flex h-screen w-screen'>
+        <Sidebar />
+        <Routes >
+          <Route path='/' element={<ChatBox />} />
+          <Route path='/credits' element={<Credits />} />
+          <Route path='/community' element={<Community />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
