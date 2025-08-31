3b7003ecda30a6b948b98d14a796be4cee292d8d
Nutileety <nutileety@gmail.com>
2025-08-31 23:24:46 +0530

    managed all the state variables in AppContext.jsx to access efficiently

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useAppContext } from '../context/AppContext'
+import { assets } from '../assets/assets';
 
 const Sidebar = () => {
+  const [ user, chats, setSelectedChat, theme, setTheme ] = useAppContext(); 
+  const [ search, setSearch ] = useState('');  
+
   return (
-    <div>Sidebar</div>
+    <div className='flex flex-col h-screen min-w-72 p-5 dark:bg-gradient-to-b 
+    from-[#242124]/30 to-[#000000]/30 border-r border-[#80609f]/30 backdrop-blur-3xl
+    transistion-all duration-500 max-md:absolute left-0 z-1'>
+      {/* logo */}
+      <img src={theme === 'dark' ? assets.logo_full : assets.logo_full_dark} alt=""
+        className='w-full max-w-48' />
+    </div>
   )
 }
 
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,10 +1,55 @@
-import { Children, createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { dummyChats, dummyUserData } from '../assets/assets'
 
 
 const AppContext = createContext();
 
 export const AppContextProvider = ({children}) => {
-    const value = {};
+    const navigate = useNavigate();
+    const [ user, setUser ] = useState(null);
+    const [ chats, setChats ] = useState([]);
+    const [selectedChat, setSelectedChat ] = useState(null);
+    const [ theme, setTheme ] = useState(localStorage.getItem('theme') || 'light');
+
+    const value = {
+        navigate, user, setUser, chats, setChats, selectedChat, setSelectedChat, theme, setTheme
+    };
+
+    //fetch user 
+    const fetchUser = async () => {
+        setUser(dummyUserData)
+    }
+
+    useEffect(() => {
+        fetchUser()
+    }, [])
+
+    //fetching user chat
+    const fetchUserChats = async() => {
+        setChats(dummyChats)
+        setSelectedChat(dummyChats[0])
+    }
+
+    useEffect(() => {
+        if(user) {
+            fetchUserChats()
+        }
+        else{
+            setChats([])
+            setSelectedChat(null)
+        }
+    }, [user])
+
+    //fetching theme
+    useEffect(() => {
+        if(theme === 'dark') {
+            document.documentElement.classList.add('dark');
+        }
+        else{
+            document.documentElement.classList.remove('dark');
+        }
+    }, [theme])
 
     return (
         <>
@@ -15,4 +60,6 @@ export const AppContextProvider = ({children}) => {
     )
 }
 
-export const useAppContext = () => useContext(AppContext);
+export function useAppContext(){
+    return useContext(AppContext)
+}
