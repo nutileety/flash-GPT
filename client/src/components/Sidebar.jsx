cc1ce261561723267be82cd124a5cd62b51e99fc
Nutileety <nutileety@gmail.com>
2025-09-05 23:28:51 +0530

    added new chat button and search input in sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -13,6 +13,21 @@ const Sidebar = () => {
       {/* logo */}
       <img src={theme === 'dark' ? assets.logo_full : assets.logo_full_dark} alt=""
         className='w-full max-w-48' />
+
+      {/* new chat */}
+      <button className='flex justify-center items-center w-full py-2 mt-8 text-white
+        bg-gradient-to-r from-[#A456F7] to-[#3D81F6] text-sm rounded-md cursor-pointer'>
+        <span className='mr-2 text-xl'>+</span> New Chat 
+        </button>
+
+      {/* search conversation */}
+      <div className='flex items-center border gap-2 p-3 mt-4 border-gray-400 rounded-md
+        dark:text-white/20 '>
+        <img src={assets.search_icon} className='w-4 opacity-50 mot-dark:invert' alt=""/>
+        <input onChange={(e) => {setSearch(e.target.value)}} value={search} type='text'
+          placeholder='search conversation' className='text-sm 
+          placeholder:text-gray-400 outline-none'/>
+      </div>
     </div>
   )
 }
